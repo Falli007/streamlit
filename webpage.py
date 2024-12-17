1bd88cb53cc4155e158d7b1e23b3a6875ac1e33e
Falli007 <allifred18@gmail.com>
2024-12-17 21:22:59 +0000

    implemented adding new todo

--- webpage.py
+++ webpage.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import functions as fn
 
+
 todos = fn.get_todos()
 
 
+def add_todo():
+    todo = st.session_state['new_todo']
+    todos.append(todo)
+    fn.write_todos(todos)
 
-
+    
 st.title ('My To Do App')
 st.subheader ('This is my To Do App')
 st.write ('This App is to increase my productivity.')
@@ -16,3 +21,4 @@ for idx, todo in enumerate(todos):
     st.checkbox(todo, key=f"checkbox_{idx}")
 
 
+st.text_input(label='', placeholder='Type your new todo here...', on_change=add_todo)
